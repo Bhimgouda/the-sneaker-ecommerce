87ac711f91af6e24fe67cee2c8969c093f8addfc
Bhimgouda <satvikp508@gmail.com>
2022-06-12 01:59:33 +0530

    without clone images and smooth transition slider

--- app.js
+++ app.js
@@ -4,42 +4,32 @@ const imageSlider = document.querySelector('.image-slider')
 const productImages = document.querySelectorAll(".image-slider img")
 
 // Buttons
-const prevBtn = document.querySelector('#prev-btn--main');
-const nextBtn = document.querySelector('#next-btn--main');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
 
 // Counter
-let counter = 1;
+let counter = 0;
 const size = productImages[0].clientWidth;
-imageSlider.style.transform = `translateX(${-size*counter}px)`;
 
 nextBtn.addEventListener('click',()=>{
-    if(counter>=productImages.length-1) return;
-    imageSlider.style.transition = 'transform 0.4s ease-in-out';
-    counter++;
-    imageSlider.style.transform = `translateX(${-size*counter}px)`;
+    if (counter<productImages.length-1){
+        counter++;
+        imageSlider.style.transition = 'transform 0.4s ease-in-out';
+        imageSlider.style.transform = `translateX(${-size*counter}px)`;
+    }
 })
+
+
 prevBtn.addEventListener('click',()=>{
-    if(counter<=0) return;
-    imageSlider.style.transition = 'transform 0.4s ease-in-out';
-    counter--;
-    imageSlider.style.transform = `translateX(${-size*counter}px)`;
-})
-
-
-
-imageSlider.addEventListener('transitionend',()=>{
-    let imageId = productImages[counter].id
-    if(imageId === 'last-clone'){
-        imageSlider.style.transition = 'none';
-        counter = productImages.length-2;
+    if(counter>0) {
+        counter--;
+        imageSlider.style.transition = 'transform 0.4s ease-in-out';
         imageSlider.style.transform = `translateX(${-size*counter}px)`;
     }
-    if(imageId === 'first-clone'){
-        imageSlider.style.transition = 'none';
-        counter = 1;
-        imageSlider.style.transform = `translateX(${-size*counter}px)`;
-    }
+
+    
 })
+
 
 
 /* Desktop image Changer */

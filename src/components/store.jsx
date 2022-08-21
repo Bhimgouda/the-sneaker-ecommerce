fc0fab88bff8841f01e7797d248c3d54c5c0bb46
Bhimgouda <satvikp508@gmail.com>
2022-08-21 22:30:17 +0530

    made a loading function

--- src/components/store.jsx
+++ src/components/store.jsx
@@ -7,6 +7,14 @@ import { getProducts } from "./../fakeDatabase";
 
 const Store = (props) => {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 500);
+  }, []);
 
   let productsInStock = [...getProducts()];
   if (props.match.params.id) {
@@ -18,14 +26,24 @@ const Store = (props) => {
     setProducts(productsInStock);
   }, [props]);
 
+  let loadingClass = "loading";
+  let className = "store__product-gallery container hide";
+  if (!loading) {
+    className = "store__product-gallery container";
+    loadingClass = "loading hide";
+  }
+
   return (
-    <div className="store__product-gallery container">
-      {products.map((product) => (
-        <Link key={product._id} to={`product/${product._id}`}>
-          <ProductInStore product={product} />
-        </Link>
-      ))}
-    </div>
+    <React.Fragment>
+      <p className={loadingClass}>Loading...</p>
+      <div className={className}>
+        {products.map((product) => (
+          <Link key={product._id} to={`product/${product._id}`}>
+            <ProductInStore product={product} />
+          </Link>
+        ))}
+      </div>
+    </React.Fragment>
   );
 };
 
